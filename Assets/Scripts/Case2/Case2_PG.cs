8566910332a884cb02f1dd09f1e8c8e1e5902adf
parkrye <parkrye@gmail.com>
2023-10-08 09:56:08 +0900

    feat: 6차 작업

--- Assets/Scripts/Case2/Case2_PG.cs
+++ Assets/Scripts/Case2/Case2_PG.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI.Table;
 
 namespace Case2
 {
@@ -24,6 +22,18 @@ namespace Case2
             position = _position;
             type = _type;
         }
+
+        public int GetAbsDistance(Structure other)
+        {
+            int xDiff = position.x - other.position.x < 0 ? other.position.x - position.x : position.x - other.position.x;
+            int yDiff = position.y - other.position.y < 0 ? other.position.y - position.y : position.y - other.position.y;
+            return xDiff + yDiff;
+        }
+
+        public int GetDistance(Structure other)
+        {
+            return other.position.x - position.x + other.position.y - position.y;
+        }
     }
 
     class PGNode
@@ -36,7 +46,8 @@ namespace Case2
         public PGNode[] Children { get { return children; } set { children = value; } }
         public List<Structure> Rooms { get { return rooms; } set { rooms = value; } }
         public bool IsRoot { get { return parent == null; } }
-        public bool IsLeaf { get { return children[0] == null; } }
+        public bool IsLeaf { get { return children[0] == null && rooms.Count > 0; } }
+        public Structure CenterRoom { get { return (rooms[rooms.Count >> 1]); } }
 
         public PGNode(PGNode _parent = null)
         {
@@ -67,6 +78,7 @@ namespace Case2
         [SerializeField][Range(1, 10)] int quantity;
         [SerializeField][Range(100, 200)] int maxRoomSize;
         float calcQuantity;
+        int areaCount;
 
         void Awake()
         {
@@ -74,6 +86,7 @@ namespace Case2
             roots = new PGNode[depth];
 
             calcQuantity = maxRoomSize - maxRoomSize * quantity * 0.1f;
+            areaCount = 0;
         }
 
         void Start()
@@ -87,7 +100,7 @@ namespace Case2
             {
                 roots[deep] = new PGNode();
                 RoomGenerate(deep, 0, size, 0, size, roots[deep]);
-                RoadGenerate(roots[deep]);
+                RoadGenerate(deep);
             }
 
             StairGenerate();
@@ -112,6 +125,7 @@ namespace Case2
                         parent.Rooms.Add(map[deep, xi, yi]);
                     }
                 }
+                areaCount++;
                 return;
             }
 
@@ -132,35 +146,98 @@ namespace Case2
             }
         }
 
-        void RoadGenerate(PGNode parent)
+        void RoadGenerate(int deep)
         {
-            if (parent.IsLeaf)
-                return;
+            PGNode now = roots[deep];
+            List<PGNode> area = new List<PGNode>(areaCount);
+            Queue<PGNode> findQueue = new Queue<PGNode>(areaCount * 2);
 
-            if (parent.Children[0].IsLeaf)
+            findQueue.Enqueue(now);
+
+            // 영역만을 선택하여 추가
+            while(findQueue.Count > 0)
             {
-                Structure roomA = parent.Children[0].Rooms[0];
-                Structure roomB = parent.Children[1].Rooms[0];
+                now = findQueue.Dequeue();
 
-                // x 차이가 0일때
-                // y 차이가 0일때
-                // 둘 다 0이 아닐 떄
+                if(now.IsLeaf)
+                {
+                    area.Add(now);
+                    continue;
+                }
 
-                int xDiff = roomA.Position.x - roomB.Position.x;
-                int yDiff = roomA.Position.y - roomB.Position.y;
-                float gradient = yDiff / xDiff;
-
-                // 변화축 값이 1 올랐을때
-                // 다른축 값이 n.5라면
-                // n, n+1은 room
-                
-                // 최소신장트리 사용 가능?!
-
-                return;
+                for(int i = 0; i < 2; i++)
+                {
+                    if (now.Children[i] != null)
+                        findQueue.Enqueue(now.Children[i]);
+                }
             }
 
-            RoadGenerate(parent.Children[0]);
-            RoadGenerate(parent.Children[1]);
+            // 영역 간의 간선 테이블
+            int[,] table = new int[areaCount, areaCount];
+            for(int i = 0; i <  areaCount; i++)
+            {
+                Structure currentCenterRoom = area[i].CenterRoom;
+                for (int j = i + 1; j < areaCount; j++)
+                {
+                    table[i, j] = currentCenterRoom.GetAbsDistance(area[j].CenterRoom);
+                }
+            }
+
+            // 최소 거리 간선 연결
+            for (int i = 0; i < areaCount; i++)
+            {
+                (int x, int y) currentRoomPosition = area[i].CenterRoom.Position;
+                int minDistance = int.MaxValue;
+                int minDistanceIndex = 0;
+                for (int j = i + 1; j < areaCount; j++)
+                {
+                    if (table[i, j] < minDistance)
+                    {
+                        minDistance = table[i, j];
+                        minDistanceIndex = j;
+                    }
+                }
+                (int x, int y) minDistanceRoomPosition = area[minDistanceIndex].CenterRoom.Position;
+                int xDiff = minDistanceRoomPosition.x - currentRoomPosition.x;
+                int yDiff = minDistanceRoomPosition.y - currentRoomPosition.y;
+                int xModifier = xDiff < 0 ? -1 : 1;
+                int yModifier = yDiff < 0 ? -1 : 1;
+
+                if(xDiff != 0 && yDiff != 0)
+                {
+                    float grad = yDiff / xDiff;
+                    for(int x = 0; x != xDiff; x += xModifier)
+                    {
+                        if (map[deep, currentRoomPosition.x + x, currentRoomPosition.y] == null)
+                            map[deep, currentRoomPosition.x + x, currentRoomPosition.y] = new Structure(deep, (currentRoomPosition.x + x, currentRoomPosition.y), StructureType.Room);
+                        for(int y = 0; y < grad && y > -grad && y >= 0 && y < size; y += yModifier)
+                        {
+                            if (map[deep, currentRoomPosition.x + x, currentRoomPosition.y + y] == null)
+                                map[deep, currentRoomPosition.x + x, currentRoomPosition.y + y] = new Structure(deep, (currentRoomPosition.x + x, currentRoomPosition.y + y), StructureType.Room);
+                        }
+                    }
+                }
+                else if(xDiff == 0 && yDiff != 0)
+                {
+                    for (int y = currentRoomPosition.y; y < minDistanceRoomPosition.y && y > -minDistanceRoomPosition.y; y += yModifier)
+                    {
+                        if (map[deep, currentRoomPosition.x, y] == null)
+                            map[deep, currentRoomPosition.x, y] = new Structure(deep, (currentRoomPosition.x, y), StructureType.Room);
+                    }
+                }
+                else if(yDiff == 0)
+                {
+                    for(int x = currentRoomPosition.x; x < minDistanceRoomPosition.x && x > -minDistanceRoomPosition.x; x += xModifier)
+                    {
+                        if (map[deep, x, currentRoomPosition.y] == null)
+                            map[deep, x, currentRoomPosition.y] = new Structure(deep, (x, currentRoomPosition.y), StructureType.Room);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
 
         void StairGenerate()
@@ -170,12 +247,15 @@ namespace Case2
 
         void CreateMap()
         {
-            for(int deep = 0; deep < depth; deep++)
+            for (int deep = 0; deep < depth; deep++)
             {
                 for (int xi = 0; xi < size; xi++)
                 {
-                    for (int yi = size; yi < size; yi++)
+                    for (int yi = 0; yi < size; yi++)
                     {
+                        if (map[deep, xi, yi] == null)
+                            continue;
+
                         switch (map[deep, xi, yi].Type)
                         {
                             default:
